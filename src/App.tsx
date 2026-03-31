f84c2cd8e1ad795a8c159a0f7486582110b90d01
[dyad] <nuramirah@ubbim.com>
2026-03-31 15:51:37 +0800

    Reverted all changes back to version 02c65bcbf418b55281bd5e6f480526879f832f12

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,52 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
 import DashboardLayout from './components/layout/DashboardLayout';
 import Index from './pages/Index';
+import ProjectDetails from './pages/projects/ProjectDetails';
+import ProjectDetailPage from './pages/projects/ProjectDetailPage';
+import Machinery from './pages/resources/Machinery';
+import Material from './pages/resources/Material';
 import Login from './pages/Login';
-import Projects from './pages/projects/Projects';
-import Inspection from './pages/projects/Inspection';
+import { AuthProvider } from './contexts/AuthContext';
+import Dashboard from './pages/Dashboard';
 import Activities from './pages/projects/Activities';
 import Deliveries from './pages/projects/Deliveries';
 import Schedules from './pages/projects/Schedules';
 import Documents from './pages/projects/Documents';
 import Issues from './pages/projects/Issues';
+import Settings from './pages/Settings';
 import Manpower from './pages/resources/Manpower';
 import ClockInOut from './pages/resources/ClockInOut';
-import Machinery from './pages/resources/Machinery';
-import Material from './pages/resources/Material';
+import Inspection from './pages/projects/Inspection';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
+    <AuthProvider>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<DashboardLayout><Index /></DashboardLayout>} />
-          <Route path="/dashboard" element={<DashboardLayout><Index /></DashboardLayout>} />
-          <Route path="/projects" element={<DashboardLayout><Projects /></DashboardLayout>} />
-          <Route path="/projects/inspection" element={<DashboardLayout><Inspection /></DashboardLayout>} />
-          <Route path="/projects/inspection/:id" element={<DashboardLayout><Inspection /></DashboardLayout>} />
-          <Route path="/projects/activities" element={<DashboardLayout><Activities /></DashboardLayout>} />
-          <Route path="/projects/deliveries" element={<DashboardLayout><Deliveries /></DashboardLayout>} />
-          <Route path="/projects/schedules" element={<DashboardLayout><Schedules /></DashboardLayout>} />
-          <Route path="/projects/documents" element={<DashboardLayout><Documents /></DashboardLayout>} />
-          <Route path="/projects/issues" element={<DashboardLayout><Issues /></DashboardLayout>} />
-          <Route path="/resources/manpower" element={<DashboardLayout><Manpower /></DashboardLayout>} />
-          <Route path="/resources/clock-in-out" element={<DashboardLayout><ClockInOut /></DashboardLayout>} />
-          <Route path="/resources/machinery" element={<DashboardLayout><Machinery /></DashboardLayout>} />
-          <Route path="/resources/material" element={<DashboardLayout><Material /></DashboardLayout>} />
+          <Route path="/" element={<Index />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<ProjectDetails />} />
+          <Route path="/projects/:id" element={<ProjectDetailPage />} />
+          <Route path="/projects/activities" element={<Activities />} />
+          <Route path="/projects/deliveries" element={<Deliveries />} />
+          <Route path="/projects/schedules" element={<Schedules />} />
+          <Route path="/projects/documents" element={<Documents />} />
+          <Route path="/projects/issues" element={<Issues />} />
+          <Route path="/projects/inspection" element={<Inspection />} />
+          <Route path="/resources/machinery" element={<Machinery />} />
+          <Route path="/resources/material" element={<Material />} />
+          <Route path="/resources/manpower" element={<Manpower />} />
+          <Route path="/resources/clock-in-out" element={<ClockInOut />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
